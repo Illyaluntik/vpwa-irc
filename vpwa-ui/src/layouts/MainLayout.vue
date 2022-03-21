2bf5711cc8ab140dd653ddd35fe85bc77664dfb2
Illia Lynnyk <illyalinnyk@gmail.com>
2022-03-21 15:00:23 +0100

    layout fixes

--- vpwa-ui/src/layouts/MainLayout.vue
+++ vpwa-ui/src/layouts/MainLayout.vue
@@ -1,9 +1,48 @@
 <template>
   <q-layout view="hHh lpR fFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
 
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+
+      </q-list>
+    </q-drawer>
     <q-page-container>
       <router-view />
     </q-page-container>
 
   </q-layout>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      leftDrawerOpen: false,
+    };
+  },
+  methods: {
+    toggleLeftDrawer() {
+      this.leftDrawerOpen = !this.leftDrawerOpen;
+    },
+  },
+});
+</script>
